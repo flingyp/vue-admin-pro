3f4b315569e6fa80bcd5629c9f829e333483ff37
flingyp <1213563369@qq.com>
2022-05-27 22:47:42 +0800

    fix: 修改样式文件名称

--- src/style/InitStyle.ts
+++ src/style/InitStyle.ts
@@ -1,20 +1,20 @@
 // 标签默认样式重置
-import './css/Reset.css'
+import './css/ResetStyle.css'
 
 // 路由过渡动画的样式
-import './css/Transition.css'
+import './css/TransitionStyle.css'
 
 // windicss文件
 import 'virtual:windi.css'
 
 // 重写NaiveUI 组件的类样式
-import './css/OverwriteNaive.css'
+import './css/OverWriteNaiveStyle.css'
 
 // NProgress 样式文件
 import 'nprogress/nprogress.css'
 
 // 重写NProgress进度条样式
-import './css/OverwriteNProgress.css'
+import './css/OverwriteNProgressStyle.css'
 
 /**
  * 初始化样式文件
